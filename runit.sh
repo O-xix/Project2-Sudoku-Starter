46afd6020aaff8041da3d8c26c3deec4194c09d5
O-xix <ichorlifephoenix@gmail.com>
2025-10-26 23:06:59 -0700

    final commit

--- runit.sh
+++ runit.sh
@@ -3,13 +3,25 @@
 # Script to compile and run sudoku program
 rm -f sudoku
 gcc -Wall -Wextra -pthread -lm -std=c99 sudoku.c -o sudoku
-./sudoku puzzle9-valid.txt
-echo "________________________________"
-./sudoku puzzle2-invalid.txt
-echo "________________________________"
 ./sudoku puzzle2-fill-valid.txt
-echo "________________________________"
+echo "________________________________puzzle2-fill-valid.txt"
+./sudoku puzzle2-invalid.txt
+echo "________________________________puzzle2-invalid.txt"
 ./sudoku puzzle2-valid.txt
+echo "________________________________puzzle2-valid.txt"
+./sudoku puzzle4-complete-invalid.txt
+echo "________________________________puzzle4-complete-invalid.txt"
+./sudoku puzzle4-solvable.txt
+echo "________________________________puzzle4-solvable.txt"
+./sudoku puzzle9-invalid-start.txt
+echo "________________________________puzzle9-invalid-start.txt"
+./sudoku puzzle9-simple-solve.txt
+echo "________________________________puzzle9-simple-solve.txt"
+./sudoku puzzle9-unsolvable.txt
+echo "________________________________puzzle9-unsolvable.txt"
+./sudoku puzzle9-valid.txt
+echo "________________________________puzzle9-valid.txt"
+
 
 # to check for memory leaks, use
 # valgrind ./sudoku puzzle9-good.txt
